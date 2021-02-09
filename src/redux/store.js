b9cbd4b31c4a3b36f3222cecf7534e62cf37e271
Mohd Maroof <mdmaroof@outlook.com>
2021-02-10 00:48:04 +0530

    root saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -25,7 +25,6 @@ export default function configureStore(initialState = {}) {
         composeEnhancers(...enhancers),
     );
 
-    sagaMiddleware.run(rootSaga);
     return store;
 }
 // export default store;
