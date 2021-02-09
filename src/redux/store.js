7c8fcf3571f9c96f08bc44edd7e2eacf50e2f115
Mohd Maroof <mdmaroof@outlook.com>
2021-02-10 00:45:35 +0530

    removing saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducers';
-import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
